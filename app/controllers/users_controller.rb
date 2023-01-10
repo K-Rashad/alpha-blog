f3f010f14176dc2559fd2ec8e9fab80074350eec
Rashad K <rashadreigns@gmail.com>
2023-01-10 14:42:04 +0530

    cleanup layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+    def index
+      @users= User.all
+    end
+
     def show
       @user = User.find(params[:id])
       @articles = @user.articles
@@ -17,7 +21,7 @@ class UsersController < ApplicationController
       @user=User.find(params[:id])
       if @user.update(user_params)
         flash[:notice] ="Your account information was successfully updated"
-        redirect_to root_path
+        redirect_to @user #we can write @user instead of user_path(@user)
       else
 
         render "edit"
