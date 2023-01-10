41329d72f8aa73d267d6376cd7fcc46df3c8b4e9
Rashad K <rashadreigns@gmail.com>
2023-01-10 11:11:20 +0530

    add user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,23 @@ class UsersController < ApplicationController
       @user = User.new
     end
 
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user=User.find(params[:id])
+      if @user.update(user_params)
+        flash[:notice] ="Your account information was successfully updated"
+        redirect_to root_path
+      else
+
+        render "edit"
+
+      end
+
+    end
+
     def create
       @user=User.new(user_params)
       if @user.save
@@ -15,6 +32,8 @@ class UsersController < ApplicationController
       end
 
     end
+
+
 
     private
 
