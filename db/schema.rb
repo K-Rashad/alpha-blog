c79838b7c77409f381c84714e8942a20900f9f08
Rashad K <rashadreigns@gmail.com>
2023-01-13 19:29:22 +0530

    edit deployment config

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_12_084323) do
+ActiveRecord::Schema.define(version: 2023_01_13_102929) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +21,12 @@ ActiveRecord::Schema.define(version: 2023_01_12_084323) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
---
+++ test/models/category_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class CategoryTest < ActiveSupport::TestCase
+  test "category should be valid" do
+    @category = Category.new(name: "Sports")
+    assert @category.valid?
+  end
+
+  test "name shoudl be present" do
+    @category = Category.new(name: " ")
+    assert_not @category.valid?
+  end
+end
